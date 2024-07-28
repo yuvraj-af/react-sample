2eed60f97af2f83ee44a0a605ba169a387da89ce
yuvraj <starkyuvraj07@gmail.com>
2024-07-28 19:32:30 +0530

    aise hi hai ji

---
+++ components/NavigationBar.jsx
@@ -0,0 +1,22 @@
+import './NavigationBar.css'
+
+
+function NavigationBar() {
+    return (
+        <div className="nav-container">
+            <div className="">
+                
+            </div>
+            <div className="menu-container">
+
+            </div>
+            <div>
+                <button >
+                    Contact Us
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default NavigationBar
